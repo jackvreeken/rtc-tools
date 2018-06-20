95ca20a51bd0287c4030ad16ffc8019307a30f40
Tjerk Vreeken <tjerk.vreeken@deltares.nl>
2018-06-20 14:22:43 +0200

    Reuse Pymoca's AliasRelation
    It makes no sense to have the same code in both RTC-Tools and Pymoca,
    especially as we depend on Pymoca. Instead we can just reuse the
    implementation of Pymoca, which also makes sure the implementations
    never get out of sync.

--- src/rtctools/_internal/alias_tools.py
+++ src/rtctools/_internal/alias_tools.py
@@ -1,5 +1,7 @@
 import collections
 
+from pymoca.backends.casadi.alias_relation import AliasRelation  # noqa: F401
+
 
 class OrderedSet(collections.MutableSet):
     """
@@ -87,82 +89,6 @@ class OrderedSet(collections.MutableSet):
             return len(self) == len(other) and list(self) == list(other)
         return set(self) == set(other)
 # End snippet
-
-
-class AliasRelation:
-    def __init__(self):
-        self._aliases = {}
-        self._canonical_variables = OrderedSet()
-
-    def add(self, a, b):
-        # Get the canonical names and signs
-        canonical_a, sign_a = self.canonical_signed(a)
-        canonical_b, sign_b = self.canonical_signed(b)
-
-        # Determine if signs need to be inverted when merging the aliased sets
-        opposite_signs = (sign_a + sign_b) == 0
-
-        # Construct aliases (a set of equivalent variables)
-        aliases = self.aliases(canonical_a)
-        for v in self.aliases(canonical_b):
-            if opposite_signs:
-                v = self.__toggle_sign(v)
-            aliases.add(v)
-
-        # Update _aliases so that keys are always positive
-        inverted_aliases = OrderedSet([self.__toggle_sign(v) for v in aliases])
-        for v in aliases:
-            if self.__is_negative(v):
-                # If v is negative, we make it positive and store the inverse alias set
-                self._aliases[self.__toggle_sign(v)] = inverted_aliases
-            else:
-                self._aliases[v] = aliases
-
-        # Update _canonical_variables with new canonical var and remove old ones
-        self._canonical_variables.add(aliases[0])
-        for v in aliases[1:]:
-            if self.__is_negative(v):
-                v = self.__toggle_sign(v)
-            try:
-                self._canonical_variables.remove(v)
-            except KeyError:
-                pass
-
-    def __toggle_sign(self, v):
-        if self.__is_negative(v):
-            return v[1:]
-        else:
-            return '-' + v
-
-    def __is_negative(self, v):
-        return True if v[0] == '-' else False
-
-    def aliases(self, a):
-        if self.__is_negative(a):
-            a = self.__toggle_sign(a)
-            return OrderedSet([self.__toggle_sign(v) for v in self._aliases.get(a, OrderedSet([a]))])
-        else:
-            return self._aliases.get(a, OrderedSet([a]))
-
-    def canonical_signed(self, a):
-        if self.__is_negative(a):
-            top_alias = self.aliases(self.__toggle_sign(a))[0]
-        else:
-            top_alias = self.aliases(a)[0]
-
-        if self.__is_negative(top_alias):
-            return top_alias[1:], 1 if self.__is_negative(a) else -1
-        else:
-            return top_alias, -1 if self.__is_negative(a) else 1
-
-    @property
-    def canonical_variables(self):
-        return self._canonical_variables
-
-    def __iter__(self):
-        return (
-            (canonical_variable, self.aliases(canonical_variable)[1:])
-            for canonical_variable in self._canonical_variables)
 
 
 class AliasDict:
