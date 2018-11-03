jackvreeken/rtc-tools

commit 80f225f900e153e7958d54fcedcc474b919497f4

Fix seed derivatives
The value of the derivatives of the initial time step and of the extra
variables is included the seed.

Closes issue #1066

src/rtctools/optimization/homotopy_mixin.py
@@ -34,6 +34,8 @@ class HomotopyMixin(OptimizationProblem):
                     # Only include seed timeseries which are consistent
                     # with the specified time stamps.
                     seed[key] = Timeseries(times, result)
+                elif len(result) == 1:
+                    seed[key] = result
         return seed
 
     def parameters(self, ensemble_member):

src/rtctools/optimization/collocated_integrated_optimization_problem.py
@@ -1749,6 +1749,15 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
                     x0[offset + k] = seed[self.extra_variables[k].name()]
                 except KeyError:
                     pass
+            offset += len(self.extra_variables)
+
+            for k in range(len(self.dae_variables['derivatives'])):
+                try:
+                    der_name = self.dae_variables['derivatives'][k].name()
+                    variable = "initial_" + der_name
+                    x0[offset + k] = seed[variable]
+                except KeyError:
+                    pass
 
         # Return number of state variables
         return count, discrete, lbx, ubx, x0, indices
@@ -1812,6 +1821,13 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
         # Extract extra variables
         for k in range(len(self.extra_variables)):
             variable = self.extra_variables[k].name()
+            results[variable] = X[offset + k].ravel()
+        offset += len(self.extra_variables)
+
+        # Extract derivatives
+        for k in range(len(self.dae_variables['derivatives'])):
+            der_name = self.dae_variables['derivatives'][k].name()
+            variable = "initial_" + der_name
             results[variable] = X[offset + k].ravel()
 
         # Done

src/rtctools/optimization/goal_programming_mixin.py
@@ -444,6 +444,8 @@ class GoalProgrammingMixin(OptimizationProblem, metaclass=ABCMeta):
                     # Only include seed timeseries which are consistent
                     # with the specified time stamps.
                     seed[key] = Timeseries(times, result)
+                elif len(result) == 1:
+                    seed[key] = result
 
         # Seed epsilons
         for epsilon in self.__subproblem_epsilons:

tests/optimization/test_goal_programming.py
@@ -808,3 +808,35 @@ class TestGoalProgrammingInvalidGoals(TestCase):
                                            target_max=Timeseries(self.problem.times(), [1.0]))]
         with self.assertRaisesRegex(Exception, "Target max cannot be a Timeseries"):
             self.problem.optimize()
+
+
+class ModelPathGoalsSeed(ModelPathGoals):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._results = []
+        self._x0 = []
+
+    def transcribe(self):
+        discrete, lbx, ubx, lbg, ubg, x0, nlp = super().transcribe()
+        self._x0.append(x0)
+
+        return discrete, lbx, ubx, lbg, ubg, x0, nlp
+
+    def priority_completed(self, priority):
+        super().priority_completed(priority)
+        self._results.append(self.solver_output)
+
+    def path_goals(self):
+        return [PathGoal3(), PathGoal4(), PathGoal5()]
+
+
+class TestGoalProgrammingSeed(TestCase):
+
+    def setUp(self):
+        self.problem = ModelPathGoalsSeed()
+        self.problem.optimize()
+
+    def test_seed(self):
+        self.assertTrue(np.array_equal(self.problem._results[0], self.problem._x0[1]))
+        self.assertTrue(np.array_equal(self.problem._results[1], self.problem._x0[2]))

commit 7055bd4bada304c6985cc3b1a36ec746c254cee2

Fix seed of HomotopyMixin
Small regression in 80f225f9.

src/rtctools/optimization/homotopy_mixin.py
@@ -30,7 +30,9 @@ class HomotopyMixin(OptimizationProblem):
             # Do not override any previously seeded values, such as goal programming results.
             for key, result in self.__results[ensemble_member].items():
                 times = self.times(key)
-                if key not in seed and len(result) == len(times):
+                if key in seed:
+                    continue
+                elif len(result) == len(times):
                     # Only include seed timeseries which are consistent
                     # with the specified time stamps.
                     seed[key] = Timeseries(times, result)