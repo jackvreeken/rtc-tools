jackvreeken/rtc-tools

commit 1c0f5f9dd7e710e1c2d9da29e9e3f12b51c57c51

Allow extra variables to appear in path expressions.
This is useful for the following type of situation.  Imagine wanting
to put a uniform upper bound for a state over all time steps.  Then,
this bound would be an extra variable, and the bound constraint would
be imposed as a path constraint.

src/rtctools/optimization/collocated_integrated_optimization_problem.py
@@ -513,6 +513,9 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
                     constant_parameters,
                     constant_parameter_values)
 
+        # Collect extra variable symbols
+        symbolic_extra_variables = ca.vertcat(*self.extra_variables)
+
         # Aggregate ensemble data
         ensemble_aggregate = {}
         ensemble_aggregate["parameters"] = ca.horzcat(*[nullvertcat(*l) for l in ensemble_parameter_values])
@@ -714,7 +717,8 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
                 *integrated_variables, *collocated_variables, *integrated_derivatives,
                 *collocated_derivatives, *self.dae_variables['constant_inputs'],
                 *self.dae_variables['time'], *self.path_variables,
-                *self.__extra_constant_inputs)]
+                *self.__extra_constant_inputs),
+            symbolic_extra_variables]
 
         # Initialize a Function for the path objective
         # Note that we assume that the path objective expression is the same for all ensemble members
@@ -770,6 +774,7 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
         collocation_time_0 = accumulated_U[offset + 0]
         collocation_time_1 = accumulated_U[offset + 1]
         path_variables_1 = accumulated_U[offset + 2:offset + 2 + len(self.path_variables)]
+        extra_variables = ca.vertcat(*[self.extra_variable(var.name()) for var in self.extra_variables])
         extra_constant_inputs_1 = accumulated_U[offset + 2 + len(self.path_variables):]
 
         # Approximate derivatives using backwards finite differences
@@ -857,7 +862,8 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
                 constant_inputs_1,
                 collocation_time_1 - t0,
                 path_variables_1,
-                extra_constant_inputs_1)]
+                extra_constant_inputs_1),
+            symbolic_extra_variables]
 
         accumulated_Y.extend(path_objective_function.call(
             self.__func_inputs_implicit, False, True))
@@ -869,7 +875,9 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
             self.__func_inputs_implicit, False, True))
 
         # Save the accumulated inputs such that can be used later in map_path_expression()
-        self.__func_accumulated_inputs = (accumulated_X, accumulated_U, symbolic_parameters)
+        self.__func_accumulated_inputs = (
+            accumulated_X, accumulated_U,
+            ca.veccat(symbolic_parameters, symbolic_extra_variables))
 
         # Use map/mapaccum to capture integration and collocation constraint generation over the
         # entire time horizon with one symbolic operation. This saves a lot of memory.
@@ -1091,11 +1099,13 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
 
             # Save these inputs such that can be used later in map_path_expression()
             self.__func_mapped_inputs.append(
-                (accumulation_X0, accumulation_U, ca.repmat(parameters, 1, n_collocation_times - 1)))
+                (accumulation_X0, accumulation_U,
+                 ca.repmat(ca.vertcat(parameters, extra_variables), 1, n_collocation_times - 1)))
 
             self.__func_initial_inputs.append([parameters, ca.vertcat(
                         initial_state, initial_derivatives, initial_constant_inputs, 0.0,
-                        initial_path_variables, initial_extra_constant_inputs)])
+                        initial_path_variables, initial_extra_constant_inputs),
+                        extra_variables])
 
             integrators_and_collocation_and_path_constraints = accumulation(
                 *self.__func_mapped_inputs[ensemble_member])
@@ -1201,7 +1211,7 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
                         initial_constant_inputs,
                         0.0,
                         [self.variable_nominal(var.name()) for var in self.path_variables],
-                        initial_extra_constant_inputs)])
+                        initial_extra_constant_inputs), extra_variables])
 
             for i in range(len(delayed_feedback_expressions)):
                 expression = delayed_feedback_expressions[i]

tests/optimization/test_modelica_mixin.py
@@ -25,6 +25,7 @@ class Model(ModelicaMixin, CollocatedIntegratedOptimizationProblem):
 
     def __init__(self):
         print(data_path())
+        self._extra_variable = MX.sym('extra')
         super().__init__(
             input_folder=data_path(),
             output_folder=data_path(),
@@ -64,6 +65,23 @@ class Model(ModelicaMixin, CollocatedIntegratedOptimizationProblem):
     def constraints(self, ensemble_member):
         # No additional constraints
         return []
+
+    @property
+    def extra_variables(self):
+        v = super().extra_variables
+        return [*v, self._extra_variable]
+
+    def bounds(self):
+        b = super().bounds()
+        b[self._extra_variable.name()] = [-1000, 1000]
+        return b
+
+    def path_constraints(self, ensemble_member):
+        c = super().path_constraints(ensemble_member)[:]
+        c.append(
+            (self.state('x') - self._extra_variable, -np.inf, 0.0)
+        )
+        return c
 
     def post(self):
         # Do
@@ -288,7 +306,14 @@ class TestModelicaMixin(TestCase, unittest.TestCase):
 
     def test_multiple_states(self):
         self.assertAlmostEqual(self.results["w"][0], 0.0, self.tolerance)
-        self.assertAlmostEqual(self.results["w"][-1], 0.5917, 1e-4)
+        self.assertAlmostEqual(self.results["w"][-1], 0.5917, 1e-3)
+
+    def test_extra_variable(self):
+        self.assertAlmostLessThan(
+            np.max(self.results["x"]),
+            self.results[self.problem._extra_variable.name()],
+            self.tolerance
+        )
 
     @unittest.skip
     def test_states_in(self):

commit 9bba722b422b104e3c7fe40c274aafb7873382ff

Col.Int.:  Fix delay function call for history data
Regression in 1c0f5f9d.

src/rtctools/optimization/collocated_integrated_optimization_problem.py
@@ -1194,14 +1194,15 @@ class CollocatedIntegratedOptimizationProblem(OptimizationProblem, metaclass=ABC
             if len(delayed_feedback_expressions) > 0:
                 delayed_feedback_history = np.zeros((history_times.shape[0], len(delayed_feedback_expressions)))
                 for i, time in enumerate(history_times):
-                    history_delayed_feedback_res = delayed_feedback_function.call(
+                    [history_delayed_feedback_res] = delayed_feedback_function.call(
                         [parameters, ca.veccat(
                             ca.transpose(history_values[i, :]),
                             ca.transpose(history_derivatives[i, :]),
                             ca.transpose(constant_input_values[i, :]),
                             time,
                             ca.repmat(np.nan, len(self.path_variables)),
-                            ca.repmat(np.nan, len(self.__extra_constant_inputs)))])
+                            ca.repmat(np.nan, len(self.__extra_constant_inputs))),
+                         ca.repmat(np.nan, len(self.extra_variables))])
                     delayed_feedback_history[i, :] = history_delayed_feedback_res
 
                 initial_delayed_feedback = delayed_feedback_function.call(