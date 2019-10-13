jackvreeken/rtc-tools

commit d77cff4ee0511f8b95850a8318e8911334861d5d

Simulation: Fix output when simulation times differ from input times

src/rtctools/simulation/csv_mixin.py
@@ -120,12 +120,14 @@ class CSVMixin(IOMixin):
         # Call parent class first for default behaviour.
         super().write()
 
+        times = self._simulation_times
+
         # Write output
         names = ['time'] + sorted(set(self.__output.keys()))
         formats = ['O'] + (len(names) - 1) * ['f8']
         dtype = {'names': names, 'formats': formats}
-        data = np.zeros(len(self.__timeseries_times), dtype=dtype)
-        data['time'] = self.__timeseries_times
+        data = np.zeros(len(times), dtype=dtype)
+        data['time'] = self.io.sec_to_datetime(times, self.io.reference_datetime)
         for variable, values in self.output.items():
             data[variable] = values
 

src/rtctools/simulation/pi_mixin.py
@@ -1,5 +1,7 @@
-import bisect
 import logging
+from datetime import timedelta
+
+import numpy as np
 
 import rtctools.data.pi as pi
 import rtctools.data.rtc as rtc
@@ -116,14 +118,19 @@ class PIMixin(IOMixin):
         # Call parent class first for default behaviour.
         super().write()
 
+        times = self._simulation_times
+        if len(set(np.diff(times))) == 1:
+            dt = timedelta(seconds=times[1] - times[0])
+        else:
+            dt = None
+
         # Start of write output
         # Write the time range for the export file.
-        t_idx = bisect.bisect_left(self.io.datetimes, self.io.reference_datetime)
-        self.__timeseries_export.times = self.__timeseries_import.times[t_idx:]
+        self.__timeseries_export.times = [self.io.reference_datetime + timedelta(seconds=s) for s in times]
 
         # Write other time settings
-        self.__timeseries_export.forecast_datetime = self.__timeseries_import.forecast_datetime
-        self.__timeseries_export.dt = self.__timeseries_import.dt
+        self.__timeseries_export.forecast_datetime = self.io.reference_datetime
+        self.__timeseries_export.dt = dt
         self.__timeseries_export.timezone = self.__timeseries_import.timezone
 
         # Write the ensemble properties for the export file.
@@ -132,8 +139,7 @@ class PIMixin(IOMixin):
 
         # For all variables that are output variables the values are
         # extracted from the results.
-        for variable in self.output_variables:
-            values = self.output[variable]
+        for variable, values in self.output.items():
             # Check if ID mapping is present
             try:
                 self.__data_config.pi_variable_ids(variable)

src/rtctools/simulation/io_mixin.py
@@ -20,9 +20,13 @@ class IOMixin(SimulationProblem, metaclass=ABCMeta):
         # Call parent class first for default behaviour.
         super().__init__(**kwargs)
 
+        self._simulation_times = []
+
     def pre(self) -> None:
         # Call read method to read all input
         self.read()
+
+        self._simulation_times = []
 
     @abstractmethod
     def read(self) -> None:
@@ -66,18 +70,19 @@ class IOMixin(SimulationProblem, metaclass=ABCMeta):
 
         logger.debug("Model inputs are {}".format(self.__input_variables))
 
+        # Set first timestep
+        self._simulation_times.append(self.get_current_time())
+
         # Empty output
         self.__output_variables = self.get_output_variables()
-        n_times = len(self.io.times_sec)
         self.__output = AliasDict(self.alias_relation)
-        self.__output.update({variable: np.full(n_times, np.nan) for variable in self.__output_variables})
 
         # Call super, which will also initialize the model itself
         super().initialize(config_file)
 
         # Extract consistent t0 values
         for variable in self.__output_variables:
-            self.__output[variable][t_idx] = self.get_var(variable)
+            self.__output[variable] = np.array([self.get_var(variable)])
 
     def __set_input_variables(self, t_idx):
         for variable in self.get_variables():
@@ -97,6 +102,7 @@ class IOMixin(SimulationProblem, metaclass=ABCMeta):
 
         # Current time stamp
         t = self.get_current_time()
+        self._simulation_times.append(t + dt)
 
         # Get current time index
         t_idx = bisect.bisect_left(self.io.times_sec, t + dt)
@@ -108,8 +114,8 @@ class IOMixin(SimulationProblem, metaclass=ABCMeta):
         super().update(dt)
 
         # Extract results
-        for variable in self.__output_variables:
-            self.__output[variable][t_idx] = self.get_var(variable)
+        for variable, values in self.__output.items():
+            self.__output[variable] = np.append(values, self.get_var(variable))
 
     @property
     def output_variables(self):

tests/simulation/test_io_mixin.py
@@ -1,4 +1,3 @@
-import bisect
 from datetime import datetime, timedelta
 
 import numpy as np
@@ -82,11 +81,9 @@ class TestDummyIOMixin(TestCase):
         self.assertEqual(self.problem.get_var('constant_output'), 0.9)  # constant_output = constant_input
         # todo add check for x_delayed once delay is properly implemented
 
-        t_idx = bisect.bisect_left(self.problem.io.datetimes, self.problem.io.reference_datetime)
-
         for output_variable in self.problem.output_variables:
             self.assertEqual(
-                self.problem.output[output_variable][t_idx],
+                self.problem.output[output_variable][0],
                 self.problem.get_var(output_variable)
             )
 
@@ -103,11 +100,9 @@ class TestDummyIOMixin(TestCase):
         self.assertEqual(self.problem.get_var('switched'), 2.0)  # 1.0 if x > 0.5 else 2.0
         self.assertEqual(self.problem.get_var('constant_output'), 1.2)  # constant_output = constant_input
 
-        t_idx = bisect.bisect_left(self.problem.io.datetimes, self.problem.io.reference_datetime)
-
         for output_variable in self.problem.output_variables:
             self.assertEqual(
-                self.problem.output[output_variable][t_idx + 1],
+                self.problem.output[output_variable][-1],
                 self.problem.get_var(output_variable)
             )
 

commit d6ee09de5525a5b66972ccc28ac9d781a8a56375

Simulation/PIMixin/CSVMixin: Fix writing output for aliases
Regression in d77cff4e, that changed the output variables to only the
canonical variables. If an aliases was desired, or multiple aliases,
these were not properly written out.

src/rtctools/simulation/csv_mixin.py
@@ -123,13 +123,13 @@ class CSVMixin(IOMixin):
         times = self._simulation_times
 
         # Write output
-        names = ['time'] + sorted(set(self.output.keys()))
+        names = ['time'] + sorted(set(self.output_variables))
         formats = ['O'] + (len(names) - 1) * ['f8']
         dtype = {'names': names, 'formats': formats}
         data = np.zeros(len(times), dtype=dtype)
         data['time'] = self.io.sec_to_datetime(times, self.io.reference_datetime)
-        for variable, values in self.output.items():
-            data[variable] = values
+        for variable in self.output_variables:
+            data[variable] = self.output[variable]
 
         fname = os.path.join(self._output_folder, self.timeseries_export_basename + '.csv')
         csv.save(fname, data, delimiter=self.csv_delimiter, with_time=True)

src/rtctools/simulation/pi_mixin.py
@@ -139,7 +139,8 @@ class PIMixin(IOMixin):
 
         # For all variables that are output variables the values are
         # extracted from the results.
-        for variable, values in self.output.items():
+        for variable in self.output_variables:
+            values = self.output[variable]
             # Check if ID mapping is present
             try:
                 self.__data_config.pi_variable_ids(variable)