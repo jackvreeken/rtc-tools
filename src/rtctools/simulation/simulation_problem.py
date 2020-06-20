jackvreeken/rtc-tools

commit 6303bd57b9044a11ae9daeccfc7d5d13e7baf1d7

Simulation: Optimize update/rootfinder call by avoiding vertcat
Vertcat calls are relatively expensive to do. The vertcat call the
prepends `dt` was therefore taking a substantial amount of time, equal
or more than the actual solving of the equations with rootfinder for
linear models.

Instead of vertcatting, we can just specify the `dt` as additional
parameter to the ca.Function that we pass to rootfinder. We can then
also just pass it separately when calling `__do_step` to do a rootfinder
step.

src/rtctools/simulation/simulation_problem.py
@@ -206,10 +206,10 @@ class SimulationProblem(DataStoreAccessor):
                 X.size1(), dae_residual.size1()))
 
         # Construct function parameters
-        parameters = ca.vertcat(dt, X_prev, *self.__sym_list[self.__states_end_index:])
+        parameters = ca.vertcat(X_prev, *self.__sym_list[self.__states_end_index:])
 
         # Construct a function res_vals that returns the numerical residuals of a numerical state
-        self.__res_vals = ca.Function("res_vals", [X, parameters], [dae_residual])
+        self.__res_vals = ca.Function("res_vals", [X, dt, parameters], [dae_residual])
 
         # Use rootfinder() to make a function that takes a step forward in time by trying to zero res_vals()
         options = {'nlpsol': 'ipopt', 'nlpsol_options': self.solver_options(), 'error_on_fail': False}
@@ -499,7 +499,7 @@ class SimulationProblem(DataStoreAccessor):
 
         # take a step
         guess = self.__state_vector[:self.__states_end_index]
-        next_state = self.__do_step(guess, ca.vertcat(dt, *self.__state_vector))
+        next_state = self.__do_step(guess, dt, self.__state_vector)
 
         # Check convergence of rootfinder
         rootfinder_stats = self.__do_step.stats()

commit 6b990ac572d6d6f5ecae6ed3f6453a808b74476b

SimulationProblem: Fix residual calculation for running with logging.DEBUG
Regression in 6303bd57. Forgot to include this particular call to
__res_vals in that commit.

src/rtctools/simulation/simulation_problem.py
@@ -526,7 +526,7 @@ class SimulationProblem(DataStoreAccessor):
 
         if logger.getEffectiveLevel() == logging.DEBUG:
             # compute max residual
-            largest_res = ca.norm_inf(self.__res_vals(next_state, ca.vertcat(self.__dt, *self.__state_vector)))
+            largest_res = ca.norm_inf(self.__res_vals(next_state, self.__dt, self.__state_vector))
             logger.debug('Residual maximum magnitude: {:.2E}'.format(float(largest_res)))
 
         # Update state vector