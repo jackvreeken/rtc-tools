fa89931baf07bd0133502447dec27e0d5c56f431
Jack Vreeken <jack@vreeken.me>
2025-11-27 19:03:35 +0100

    interpolation: Fix .* import in __init__
    Note that we disabled the F403 warnings by excluding the file from
    pre-commit checks. With the fixes in this commit, we can re-enable the
    formatting checks again.
    
    Signed-off-by: Jack Vreeken <jack@vreeken.me>

--- src/rtctools/data/interpolation/__init__.py
+++ src/rtctools/data/interpolation/__init__.py
@@ -1,3 +1,5 @@
-from .bspline import *
-from .bspline1d import *
-from .bspline2d import *
+from .bspline import BSpline
+from .bspline1d import BSpline1D
+from .bspline2d import BSpline2D
+
+__all__ = ["BSpline", "BSpline1D", "BSpline2D"]
